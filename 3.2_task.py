5ae77fceb46772ec9e90440f8b225e99b432bf86
Przemysław Kawecki <przemysl.kawecki@gmail.com>
2021-07-05 21:55:03 +0200

    Init priject

--- 3.2_task.py
+++ 3.2_task.py
@@ -11,11 +11,3 @@ shopping_dict = {
 for shop, product in shopping_dict.items():
         print(f'idę do {shop.capitalize()}, kupuję tam następujące rzeczy: {product}')
 print ("W sumie kupię",len(shopping_dict_list1+shopping_dict_list2),"produktów")
-for numbers in range (0,101):
-        if numbers%5==0:
-                pass
-                if numbers!=0:
-                        print(numbers)
-list_numbers = [numbers **3 for numbers in range(0,101) if numbers%5==0]
-list_numbers.remove(0)
-print(list_numbers)
